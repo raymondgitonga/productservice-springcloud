35ead809a7abc7552fa95b6673105b602ed849fb
raymond gitonga <raytosh95@gmail.com>
2020-03-29 06:31:10 +0300

    Add Ribbon rest client

--- src/main/java/com/tosh/productservice/restclient/CouponClient.java
+++ src/main/java/com/tosh/productservice/restclient/CouponClient.java
@@ -1,11 +1,13 @@
 package com.tosh.productservice.restclient;
 
 import com.tosh.productservice.model.Coupon;
+import org.springframework.cloud.netflix.ribbon.RibbonClient;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient("COUPON-SERVICE")
+@RibbonClient("COUPON-SERVICE")
 public interface CouponClient {
 
     @GetMapping("/couponapi/coupons/{code}")
