926b3165e3d14418a74604cef59b98c8b0f14408
raymond gitonga <raytosh95@gmail.com>
2020-03-29 03:14:49 +0300

    Initial commit

---
+++ src/test/java/com/tosh/productservice/ProductserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tosh.productservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
